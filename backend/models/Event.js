611fc694bc133ae3577f4b5e885b8475831b4490
Lillebrorgroda <therese.modig@hotmail.com>
2025-08-29 06:39:40 +0200

    Fixed toggleTodo

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -6,6 +6,15 @@ const eventSchema = new mongoose.Schema({
   time: { type: String },
   text: { type: String, required: true },
   done: { type: Boolean, default: false }
+}, {
+  toJSON: {
+    transform: function (doc, ret) {
+      ret.id = ret._id
+      delete ret._id
+      delete ret.__v
+      return ret
+    }
+  }
 })
 
 export default mongoose.model("Event", eventSchema)
